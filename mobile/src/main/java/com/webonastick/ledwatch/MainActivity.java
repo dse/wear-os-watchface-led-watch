d02846f5d11bb0859dc29ceade94e23a4717b219
Darren Embry <dse@webonastick.com>
2019-09-18 19:09:59 -0400

    phone screen works now

--- mobile/src/main/java/com/webonastick/ledwatch/MainActivity.java
+++ mobile/src/main/java/com/webonastick/ledwatch/MainActivity.java
@@ -1,7 +1,9 @@
 package com.webonastick.ledwatch;
 
+import android.content.pm.PackageInfo;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.versionNumberTextView);
+        try {
+            PackageInfo pInfo = getApplicationContext().getPackageManager().getPackageInfo(getPackageName(), 0);
+            String versionName = pInfo.versionName;
+            int versionCode = pInfo.versionCode;
+            textView.setText(versionName + " (" + versionCode + ")");
+        } catch (Exception e) {
+            textView.setText("???.???");
+        }
     }
 }
